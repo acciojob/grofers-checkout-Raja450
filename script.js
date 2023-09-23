34853cbd5f7b33ddd4c9f28e0902b1c30a5c8470
Raja450 <69240883+Raja450@users.noreply.github.com>
2023-09-23 09:29:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,25 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+  // Select all elements with class "prices"
+  const priceElements = document.querySelectorAll(".prices");
+  let total = 0;
+
+  // Loop through price elements and sum their values
+  priceElements.forEach((element) => {
+    total += parseFloat(element.textContent); // Convert to float for accurate sum
+  });
+
+  // Create a new row for the total
+  const newRow = document.createElement("tr");
+  const totalCell = document.createElement("td");
+  totalCell.setAttribute("colspan", "2"); // Span the entire width of the table
+  totalCell.textContent = "Total Price: Rs " + total.toFixed(2); // Display the total price
+  newRow.appendChild(totalCell);
+
+  // Append the new row to the table
+  const table = document.querySelector("table");
+  table.appendChild(newRow);
 };
 
 getSumBtn.addEventListener("click", getSum);
-
